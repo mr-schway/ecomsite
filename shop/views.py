a83781a52d877db0c49892d8516c41197090a708
Shafiqullah Qasemi <shafiqullah.qsm@gmail.com>
2025-05-08 12:39:34 +0300

    Build index

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from shop.models import Products
+
 
 # Create your views here.
+def index(request):
+  productObjects = Products.objects.all()
+  return render(request, 'shop/index.html', {'productObjects': productObjects})
