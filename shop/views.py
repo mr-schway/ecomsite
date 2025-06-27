c8fd47af093841d1179cef37c3c3568f8b31f23f
Shafiqullah Qasemi <shafiqullah.qsm@gmail.com>
2025-06-27 15:00:50 +0300

    Add search functionality

--- shop/views.py
+++ shop/views.py
@@ -5,4 +5,8 @@ from shop.models import Products
 # Create your views here.
 def index(request):
   productObjects = Products.objects.all()
+
+  itemName = request.GET.get('itemName')
+  if itemName != '' and itemName is not None:
+    productObjects = productObjects.filter(title__icontains = itemName)
   return render(request, 'shop/index.html', {'productObjects': productObjects})
